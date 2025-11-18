4e5892951809532a58de18f35c48547d8ee109f0
Maksim Peskov <kostarsf@gmail.com>
2025-11-18 13:39:34 +0300

    Implement PlayerStats component to display player information and server time prediction using Zustand state management

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,39 @@
 import "./index.css";
+import { useStats } from "./store";
 
 export function App() {
   return (
-    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <h1>Hello World</h1>
+    <div className="p-4 z-10">
+      <PlayerStats />
+    </div>
+  );
+}
+
+function PlayerStats() {
+  const stats = useStats();
+
+  const predictedServerTime = Math.floor(performance.now() + stats.offset);
+
+  return (
+    <div>
+      <p className="text-sm">Player ID: {stats.playerId}</p>
+      <p className="text-sm">Rotation: {stats.playerObject?.rotation}</p>
+      <p className="text-sm">
+        Position: {stats.playerObject?.x}, {stats.playerObject?.y}
+      </p>
+      {stats.hasTimeSync ? (
+        <>
+          <p className="text-xs text-gray-500">
+            Ping: {Math.floor((stats.latency / 2) * 100) / 100}
+          </p>
+          <p className="text-xs text-gray-500">
+            Offset: {Math.floor(stats.offset * 100) / 100}
+          </p>
+          <p className="text-xs text-gray-500">
+            Server Time Prediction: {predictedServerTime}
+          </p>
+        </>
+      ) : null}
     </div>
   );
 }
